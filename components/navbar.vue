e33c0037682dd5fb7615b2ff7d76182ee15f7900
sergiodavid432 <sergiodavid432@gmail.com>
2025-01-20 13:25:31 +0000

    agregue componentes para el chatbox + tailwinds + index.vue

---
+++ components/navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="bg-white border-gray-200">
+    <ul class="flex space-x-4">
+      <li><NuxtLink to="/" class="text-gray-800 hover:text-blue-500 font-medium mr-4">Inicio</NuxtLink></li>
+      <li><NuxtLink to="/about" class="text-gray-800 hover:text-blue-500 font-medium mr-4">Acerca de</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-center">
+      <h1 class="text-3xl font-bold">bockstack</h1>
+      <Navbar />
+      <Chatbox />
+      
+    </div>
+  </template>
+  
