296dc53d8cd17a520af9fa1a080f286de723a6e1
julianmorenoc1 <julianmorenoc1@gmail.com>
2025-01-22 14:23:40 +0000

    se corrigen errores de estilos

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  buildModules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  compatibilityDate: '2025-01-22',
 });
